4b23cec42068aa14173408c7570f71a17a748c36
Aliza Shaikh <112341788+git-aliza111@users.noreply.github.com>
2022-09-09 12:59:56 +0530

    servlets

---
+++ Golden Web/src/java/SessionDemo.java
@@ -0,0 +1,39 @@
+package com;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionDemo
+ */
+@WebServlet("/SessionDemo")
+public class SessionDemo extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session=request.getSession();
+		ServletOutputStream out=response.getOutputStream();
+		Object obj=session.getAttribute("count");
+		if(obj==null) {
+			out.println("<h1> Welcome for the first time client..</h1>");
+			session.setAttribute("count",1);
+		}
+		else {
+			int c=Integer.parseInt(obj.toString());
+			c++;
+			out.println("<h1>"+" you are visiting "+c+ " time"+"</h1>");
+			session.setAttribute("count", c);
+		}
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
